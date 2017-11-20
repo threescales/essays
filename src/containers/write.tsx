5293746d307f8533b487c7a66b5f022aa73ce18c
张有明 <3142717@qq.com>
2017-11-20 14:41:42 +0800

    add block and function

--- src/containers/write.tsx
+++ src/containers/write.tsx
@@ -9,6 +9,7 @@ import { Serlizer } from "../components/editor/utils/serializer";
 import { createImagePlugin } from "../components/editor/plugins/image/index";
 import createAlignmentPlugin from "draft-js-alignment-plugin";
 import createFocusPlugin from "draft-js-focus-plugin";
+import createBlockDndPlugin from "draft-js-drag-n-drop-plugin";
 import linkify from "draft-js-linkify-plugin";
 import createCodePlugin from "app/components/editor/plugins/code-highlight/code-light.plugin";
 import createInlineToolbarPlugin from "draft-js-inline-toolbar-plugin";
@@ -20,13 +21,18 @@ import createColorBlockPlugin from "../components/editor/plugins/focusColor/inde
 
 const focusPlugin = createFocusPlugin();
 const alignmentPlugin = createAlignmentPlugin();
+const blockDndPlugin = createBlockDndPlugin();
+
 const { AlignmentTool } = alignmentPlugin;
 
 const decorator = composeDecorators(
     alignmentPlugin.decorator,
     focusPlugin.decorator,
+    blockDndPlugin.decorator
 );
 const colorBlockPlugin = createColorBlockPlugin({ decorator })
+
+
 
 const inlineToolbarPlugin = createInlineToolbarPlugin();
 const { InlineToolbar } = inlineToolbarPlugin;
@@ -44,6 +50,7 @@ const plugins = [
     catalouePlugin,
     focusPlugin,
     alignmentPlugin,
+    blockDndPlugin,
     colorBlockPlugin,
     imagePlugin,
     linkify(),
