6e6e98456bb2401e3a1767d995bb8b4a0eecaf76
张有明 <3142717@qq.com>
2017-11-28 13:46:19 +0800

    create new article optimize

--- src/components/modal/createNewArticle.tsx
+++ src/components/modal/createNewArticle.tsx
@@ -63,9 +63,9 @@ export default class CreateNewArticle extends React.Component<ICreateNewArticleP
             <div>
                 <button onClick={this.openModal}>创建文章</button>
                 <Modal isOpen={this.state.modalIsOpen} contentLabel="创建文章">
-                    <InputLabel value={this.state.title} onChange={this.toggleTitle} placeholder="请输入文章标题" label="标题" />
-                    <TextareaLabel placeholder="请输入文章简介" onChange={this.toggleDescription} value={this.state.description} label="简介" />
-                    <InputLabel value={this.state.tag} onChange={this.toggleTag} placeholder="请输入文章标签" label="标题" />
+                    <InputLabel value={this.state.title} onChange={this.toggleTitle} placeholder="请输入文章标题" label="标题：" />
+                    <TextareaLabel placeholder="请输入文章简介" onChange={this.toggleDescription} value={this.state.description} label="简介：" />
+                    <InputLabel value={this.state.tag} onChange={this.toggleTag} placeholder="请输入文章标签" label="标签：" />
                     <div style={{ width: '400px', height: '200px' }}>
                         <Background isEditable={true} uploadFinishCallback={this.toggleCover} imageUrl={this.state.cover} />
                     </div>
