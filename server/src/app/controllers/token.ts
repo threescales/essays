89d8ceff79b49e6bfd1d4be5c93f7f0d6490b41f
张有明 <3142717@qq.com>
2017-12-11 20:18:19 +0800

    access_token console

--- server/src/app/controllers/token.ts
+++ server/src/app/controllers/token.ts
@@ -33,7 +33,7 @@ export default class TokenController {
     let data = await rq(url, options)
     console.log(data)
     let access_token = data.access_token
-    console.log(access_token);
+    console.log(`github access_tokent is:${access_token}`);
     let userData = await rq(`https://api.github.com/user?access_token=${access_token}`)
     console.log(userData)
   }
