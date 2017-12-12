a2dadab8564bda2d5c541b52e3fa69134ea30416
张有明 <3142717@qq.com>
2017-12-12 12:32:59 +0800

    outh optimize

--- server/src/utils/getOAuthUrl.ts
+++ server/src/utils/getOAuthUrl.ts
@@ -1,4 +1,4 @@
 const config = require('../app/controllers/token.json')
 export function getGithubUrl() {
-    return `https://github.com/login/OAuth/authorize?client_id=${config.github_client_id}`
+    return `https://github.com/login/OAuth/authorize?client_id=${config.github_client_id}&scope=user.email`
 }
