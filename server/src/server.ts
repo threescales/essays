c0ce1771301b22e42a9763c985e04a277e73873f
张有明 <3142717@qq.com>
2018-02-06 15:57:49 +0800

    add koa-cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,55 +1,54 @@
-import * as Koa from 'koa'
-import json = require('koa-json')
-import compress = require('koa-compress')
-import logger = require('koa-logger')
-import staticServer = require('koa-static')
-import * as path from 'path'
-import routerFactory from './router'
-import http = require('http')
-import helmet = require('koa-helmet')
-import bodyParser = require('koa-bodyparser')
-import config = require('config')
-import logMiddleWare from './middlewares/logger'
-import { updateCSRFToken, CSRFSettings } from './middlewares/csrf'
-import sessionSettings from './middlewares/session'
-import koaViews = require('koa-views')
-import session = require('koa-session')
-import limit = require('koa-limit');
+import * as Koa from "koa";
+import json = require("koa-json");
+import compress = require("koa-compress");
+import logger = require("koa-logger");
+import staticServer = require("koa-static");
+import * as path from "path";
+import routerFactory from "./router";
+import http = require("http");
+import helmet = require("koa-helmet");
+import bodyParser = require("koa-bodyparser");
+import config = require("config");
+import logMiddleWare from "./middlewares/logger";
+import { updateCSRFToken, CSRFSettings } from "./middlewares/csrf";
+import sessionSettings from "./middlewares/session";
+import koaViews = require("koa-views");
+import session = require("koa-session");
+import limit = require("koa-limit");
+// var cors = require("koa-cors");
 
-import { read } from 'fs';
+import { read } from "fs";
 
 const app = new Koa();
 const router = routerFactory();
-app.keys = ['a', 'b']
+app.keys = ["a", "b"];
 
-
-
-
-
-app.use(function* (next) {
+app.use(function*(next) {
   try {
     yield next;
   } catch (err) {
-    console.log(err.stack)
-    this.throw(err.status || 500, JSON.stringify({ errMsg: err.message }))
+    console.log(err.stack);
+    this.throw(err.status || 500, JSON.stringify({ errMsg: err.message }));
   }
 });
 
 // tslint:disable-next-line:jsdoc-format
 /**  use middleware*/
 
-app.use(helmet())
-app.use(logger())
-app.use(limit({
-  limit: 10,
-  interval: 1000 * 5
-}))
+app.use(helmet());
+app.use(logger());
+app.use(
+  limit({
+    limit: 10,
+    interval: 1000 * 5
+  })
+);
 /**
  * Add session support
  */
 app.use(session(sessionSettings, app));
 
-app.use(bodyParser())
+app.use(bodyParser());
 /**
  *  Add the CSRF middleware
  */
@@ -60,21 +59,20 @@ app.use(updateCSRFToken);
  */
 
 // koa-views Must be used before any router is used
-app.use(koaViews(__dirname + '/app/views', { extension: 'pug' }))
+app.use(koaViews(__dirname + "/app/views", { extension: "pug" }));
 
-app.use(compress())
+app.use(compress());
 app.use(json());
 app.use(router.routes());
 
-app.use(staticServer(__dirname + './../../public', {
-}))
+app.use(staticServer(__dirname + "./../../public", {}));
 
 //Hooks
-app.on('error', function (err) {
-  this.status = 200
+app.on("error", function(err) {
+  this.status = 200;
   this.body = {
     message: err.message
-  }
-})
-console.log(__dirname)
+  };
+});
+console.log(__dirname);
 export = app;
--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,12 @@ module.exports = {
     devServer: {
         historyApiFallback: { index: "/" },
         disableHostCheck: true,
+        // inline: true
+        // headers: {
+        //     "Access-Control-Allow-Origin": "*",
+        //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        //     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        // }
         proxy: Object.assign({}, proxy(), {
             "*": "//localhost:3000",
             "*.hot-update.json": { ignorePath: true }
