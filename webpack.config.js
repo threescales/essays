83152e2558b2a80f95c988cf6ec34db79b119360
张有明 <3142717@qq.com>
2018-02-06 15:17:20 +0800

    header border ui optimized

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,72 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const proxy = require('./proxy/webpack-dev-proxy');
-const loaders = require('./webpack/loaders');
-const plugins = require('./webpack/plugins');
-const localIp = require('ip').address()
-const config = require('./config/qiniu.json')
-    // const applicationEntries = process.env.NODE_ENV === 'development'
-    //   ? ['webpack-hot-middleware/client?reload=true']
-    //   : [];
+const path = require("path");
+const proxy = require("./proxy/webpack-dev-proxy");
+const loaders = require("./webpack/loaders");
+const plugins = require("./webpack/plugins");
+const localIp = require("ip").address();
+const config = require("./config/qiniu.json");
+// const applicationEntries = process.env.NODE_ENV === 'development'
+//   ? ['webpack-hot-middleware/client?reload=true']
+//   : [];
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const productionPublicPath = process.env.CDN_PATH ?
     process.env.CDN_PATH + "/" + config.path + "/" :
-    `//localhost:${PORT ? PORT : 3000}/`
+    `//localhost:${PORT ? PORT : 3000}/`;
 
 module.exports = {
     entry: {
-        index: './src/index.tsx',
+        index: "./src/index.tsx"
     },
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, "dist"),
 
-        filename: process.env.NODE_ENV === 'production' ?
-            '[name][hash].js' : '[name].js',
+        filename: process.env.NODE_ENV === "production" ? "[name][hash].js" : "[name].js",
 
-        publicPath: process.env.NODE_ENV === 'production' ?
-            productionPublicPath : `//${localIp}:8080/`,
+        publicPath: process.env.NODE_ENV === "production" ?
+            productionPublicPath :
+            `//${localIp}:8080/`,
 
-        sourceMapFilename: process.env.NODE_ENV === 'production' ?
-            '[name][hash].js.map' : '[name].js.map',
+        sourceMapFilename: process.env.NODE_ENV === "production" ?
+            "[name][hash].js.map" :
+            "[name].js.map",
 
-        chunkFilename: '[id][hash].js',
+        chunkFilename: "[id][hash].js"
     },
 
-    devtool: process.env.NODE_ENV === 'production' ?
-        'source-map' : 'cheap-module-eval-source-map',
+    devtool: process.env.NODE_ENV === "production" ?
+        "source-map" :
+        "cheap-module-eval-source-map",
 
     resolve: {
         alias: {
-            'react': path.join(__dirname, 'node_modules', 'react'),
-            'utils': path.join(__dirname, 'src', 'utils'),
-            'app': path.join(__dirname, 'src'),
+            react: path.join(__dirname, "node_modules", "react"),
+            utils: path.join(__dirname, "src", "utils"),
+            app: path.join(__dirname, "src")
         },
 
         extensions: [
-            '.webpack.js',
-            '.web.js',
-            '.tsx',
-            '.ts',
-            '.js',
-            '.jsx',
-            '.json',
-        ],
+            ".webpack.js",
+            ".web.js",
+            ".tsx",
+            ".ts",
+            ".js",
+            ".jsx",
+            ".json"
+        ]
     },
 
     plugins,
 
     devServer: {
-        historyApiFallback: { index: '/' },
+        historyApiFallback: { index: "/" },
         disableHostCheck: true,
         proxy: Object.assign({}, proxy(), {
-            '*': '//localhost:3000',
-            '*.hot-update.json': { ignorePath: true },
-        }),
+            "*": "//localhost:3000",
+            "*.hot-update.json": { ignorePath: true }
+        })
     },
 
     module: {
@@ -82,16 +84,15 @@ module.exports = {
             loaders.ttf,
             loaders.json,
             loaders.img
-        ],
+        ]
     },
 
     externals: {
-        'react/lib/ReactContext': 'window',
-        'react/lib/ExecutionEnvironment': true,
-        'react/addons': true,
+        "react/lib/ReactContext": "window",
+        "react/lib/ExecutionEnvironment": true,
+        "react/addons": true,
         // why this
         // https://github.com/davezuko/react-redux-starter-kit/issues/327
-        fs: '{}',
-    },
-
+        fs: "{}"
+    }
 };
