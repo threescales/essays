a5d32121a5c3983d854edfc636b5753509fc1cb6
张有明 <3142717@qq.com>
2018-02-08 10:08:36 +0800

    page script optimized

--- server/src/utils/staticSource.ts
+++ server/src/utils/staticSource.ts
@@ -1,93 +1,91 @@
-import { info, error } from '../app/components/log/index';
-import { readFileSync, existsSync } from 'fs'
-import { join } from 'path'
-const manifestPath = join(__dirname, '../../../../dist/essays-manifest.json')
-const scripts = ['common-in-lazy.js', 'vendor.js', 'index.js',]
-const css = ['index.css']
-let cache
-let fileHash
+import { info, error } from "../app/components/log/index";
+import { readFileSync, existsSync } from "fs";
+import { join } from "path";
+const manifestPath = join(__dirname, "../../../../dist/essays-manifest.json");
+const scripts = ["common-in-lazy.js", "vendor.js", "index.js"];
+const css = ["index.css"];
+let cache;
+let fileHash;
 
 export function getSource() {
   if (cache) {
-    return cache
+    return cache;
   }
 
-  const isProduction = process.env.NODE_ENV === 'production'
-  info('NODE_ENV:' + process.env.NODE_ENV)
+  const isProduction = process.env.NODE_ENV === "production";
+  info("NODE_ENV:" + process.env.NODE_ENV);
 
   if (isProduction) {
-    cache = findFilesForProduction()
-    return cache
+    cache = findFilesForProduction();
+    return cache;
   } else {
     cache = {
-      scripts: ['http://localhost:8080/index.js'],
+      scripts: ["//localhost:8080/index.js"],
       css: []
-    }
-    return cache
+    };
+    return cache;
   }
 }
 
-
 function findFilesForProduction() {
-  const hasFile = existsSync(manifestPath)
+  const hasFile = existsSync(manifestPath);
   let resource = {
     css: [],
     scripts: []
-  }
+  };
 
   if (hasFile) {
-    const file = readFileSync(manifestPath)
-    const json = JSON.parse(file.toString())
+    const file = readFileSync(manifestPath);
+    const json = JSON.parse(file.toString());
     if (!fileHash) {
-      fileHash = getFileHash(json)
+      fileHash = getFileHash(json);
     }
 
-    const s = scripts.map(s => getPublicPath(json[s]))
-    const style = css.map(s => getPublicPath(json[s]))
-    resource.scripts = s
-    resource.css = style
-    info(s)
-    info(style)
+    const s = scripts.map(s => getPublicPath(json[s]));
+    const style = css.map(s => getPublicPath(json[s]));
+    resource.scripts = s;
+    resource.css = style;
+    info(s);
+    info(style);
 
-    return resource
+    return resource;
   } else {
-    throw new Error(`${manifestPath} must exist`)
+    throw new Error(`${manifestPath} must exist`);
   }
 }
 
 function getPublicPath(fileName: string) {
-  const isLocal = process.env.LOCAL === 'local'
+  const isLocal = process.env.LOCAL === "local";
   if (!fileName) {
-    error('file not exist:' + fileName)
+    error("file not exist:" + fileName);
   }
 
   if (!fileHash || fileHash.length !== 20) {
-    throw 'Incorrect webpack file hash'
+    throw "Incorrect webpack file hash";
   }
 
   if (isLocal) {
-    const path = '//localhost:3000/'
-    return path + fileName
+    const path = "//localhost:3000/";
+    return path + fileName;
   } else {
-    const path = process.env.CDN_HOST
-    if (!path || !path.startsWith('//') && !path.startsWith('//')) {
-      const errorMessage = 'you should define process.env.CDN_HOST startswith http(s):// in production'
-      error(errorMessage)
-      throw errorMessage
+    const path = process.env.CDN_HOST;
+    if (!path || (!path.startsWith("//") && !path.startsWith("//"))) {
+      const errorMessage =
+        "you should define process.env.CDN_HOST startswith http(s):// in production";
+      error(errorMessage);
+      throw errorMessage;
     } else {
-      return path + `/assets/` + fileName
+      return path + `/assets/` + fileName;
     }
   }
 }
 
 function getFileHash(json) {
   if (!json) {
-    return
+    return;
   }
-  const indexjs = scripts[2]
-  return json[indexjs]
-    .replace(/index/, '')
-    .replace(/\.js/, '')
+  const indexjs = scripts[2];
+  return json[indexjs].replace(/index/, "").replace(/\.js/, "");
 }
 
-getSource()
+getSource();
